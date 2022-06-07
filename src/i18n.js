1cf9238ca55eead44b24021b8fcae51257f98998
Sora Arakawa <mail@arkw.net>
2022-06-07 14:26:21 +0900

    Create i18n.js

---
+++ src/i18n.js
@@ -0,0 +1,35 @@
+module.exports = {
+  ja_JP: {
+    message: {
+      dialogClose: "閉じる",
+      optionTitle: "設定",
+      optionTheme: "テーマ",
+      optionLanguage: "言語",
+      versionTitle: "Ahoxa について",
+      updateTitle: "新しいAhoxaを利用可能",
+      updateDescription: "ここをタップして再読み込み",
+    },
+  },
+  en_US: {
+    message: {
+      dialogClose: "Close",
+      optionTitle: "Settings",
+      optionTheme: "Theme",
+      optionLanguage: "Language",
+      versionTitle: "About Ahoxa",
+      updateTitle: "New version of Ahoxa is available",
+      updateDescription: "Tap here to reload",
+    },
+  },
+  zh_CN: {
+    message: {
+      dialogClose: "关闭",
+      optionTitle: "设定",
+      optionTheme: "外观",
+      optionLanguage: "语言",
+      versionTitle: "关于 Ahoxa",
+      updateTitle: "可以使用新的Ahoxa",
+      updateDescription: "点这里重新加载",
+    },
+  },
+};
--- src/main.js
+++ src/main.js
@@ -4,44 +4,9 @@ import axios from "axios";
 import VueAxios from "vue-axios";
 import { createI18n } from "vue-i18n";
 
-const messages = {
-  ja_JP: {
-    message: {
-      dialogClose: "閉じる",
-      optionTitle: "設定",
-      optionTheme: "テーマ",
-      optionLanguage: "言語",
-      versionTitle: "Ahoxa について",
-      updateTitle: "新しいAhoxaを利用可能",
-      updateDescription: "ここをタップして再読み込み",
-    },
-  },
-  en_US: {
-    message: {
-      dialogClose: "Close",
-      optionTitle: "Settings",
-      optionTheme: "Theme",
-      optionLanguage: "Language",
-      versionTitle: "About Ahoxa",
-      updateTitle: "New version of Ahoxa is available",
-      updateDescription: "Tap here to reload",
-    },
-  },
-  zh_CN: {
-    message: {
-      dialogClose: "关闭",
-      optionTitle: "设定",
-      optionTheme: "外观",
-      optionLanguage: "语言",
-      versionTitle: "关于 Ahoxa",
-      updateTitle: "可以使用新的Ahoxa",
-      updateDescription: "点这里重新加载",
-    },
-  },
-};
-
+const messages = require("./i18n");
 const i18n = createI18n({
-  locale: "zh_CN",
+  locale: "ja_JP",
   messages,
 });
 
