cbaab335d1c50b5326d6a4d4664e04473fc58078
Sora Arakawa <mail@arkw.net>
2022-06-07 14:23:11 +0900

    Add en_US and zh_CN locale

--- src/main.js
+++ src/main.js
@@ -16,10 +16,32 @@ const messages = {
       updateDescription: "ここをタップして再読み込み",
     },
   },
+  en_US: {
+    message: {
+      dialogClose: "Close",
+      optionTitle: "Settings",
+      optionTheme: "Theme",
+      optionLanguage: "Language",
+      versionTitle: "About Ahoxa",
+      updateTitle: "New version of Ahoxa is available",
+      updateDescription: "Tap here to reload",
+    },
+  },
+  zh_CN: {
+    message: {
+      dialogClose: "关闭",
+      optionTitle: "设定",
+      optionTheme: "外观",
+      optionLanguage: "语言",
+      versionTitle: "关于 Ahoxa",
+      updateTitle: "可以使用新的Ahoxa",
+      updateDescription: "点这里重新加载",
+    },
+  },
 };
 
 const i18n = createI18n({
-  locale: "ja_JP",
+  locale: "zh_CN",
   messages,
 });
 
